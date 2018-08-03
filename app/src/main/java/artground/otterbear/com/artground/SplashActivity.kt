670c9fc7a314d584232149cd126fc5247426f169
DravenLee <incic@naver.com>
2018-08-04 00:15:05 +0900

    solve conflict

--- app/src/main/java/artground/otterbear/com/artground/SplashActivity.kt
+++ app/src/main/java/artground/otterbear/com/artground/SplashActivity.kt
@@ -8,6 +8,6 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        //working
+        //working1231233
     }
 }
