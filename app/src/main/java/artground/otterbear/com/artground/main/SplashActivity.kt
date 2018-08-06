57cf8e1a5e65895869ba1bbd6e77f640b236f607
sumin jin <pistolcaffe@naver.com>
2018-08-06 17:43:47 +0900

    프로젝트 초기 세팅 1. 필요 라이브러리 gradle dependency 추가 - glide, android support library, gson, lifecycle (ViewModel, LiveData), room database 2. SplashActivity Manifest 에 등록 3. 앱 테마 색상 설정 (941e34 : 서울시 공식 색상)

---
+++ app/src/main/java/artground/otterbear/com/artground/main/SplashActivity.kt
@@ -0,0 +1,15 @@
+package artground.otterbear.com.artground.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import artground.otterbear.com.artground.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        //working1231233
+        //branch test
+    }
+}
