7bd76155e79895c252b68729c798904309d31d85
DravenLee <incic@naver.com>
2018-09-04 04:52:19 +0900

    conflict solve
    splash ,Preperation 중간  Commit 코드 정리중
    
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/artground/otterbear/com/artground/main/MainActivity.kt
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/dimens.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml

---
+++ app/src/main/java/artground/otterbear/com/artground/main/PreperationActivity.kt
@@ -0,0 +1,52 @@
+package artground.otterbear.com.artground.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import artground.otterbear.com.artground.R
+import artground.otterbear.com.artground.preperationfragment.PreperationFragment1
+import artground.otterbear.com.artground.preperationfragment.PreperationFragment2
+import kotlinx.android.synthetic.main.activity_preperation.*
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.view.ViewPager
+import android.widget.TextView
+import artground.otterbear.com.artground.preperationfragment.PreperationFragment3
+
+
+class PreperationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preperation)
+        setSupportActionBar(toolbar)
+
+        val vp = findViewById(R.id.preperation_vp) as ViewPager
+        vp.setAdapter(pagerAdapter(supportFragmentManager))
+        vp.setCurrentItem(0)
+
+    }
+
+    private class pagerAdapter : FragmentStatePagerAdapter {
+        constructor(fm: android.support.v4.app.FragmentManager) : super(fm)
+
+        override fun getItem(position: Int): android.support.v4.app.Fragment? {
+            when (position) {
+                0 -> {
+                    return PreperationFragment1()
+                }
+                1 -> {
+                    return PreperationFragment2()
+                }
+                2 -> {
+                    return PreperationFragment3()
+                }
+                else -> {
+                    return null
+                }
+            }
+        }
+
+        override fun getCount(): Int {
+            return 3
+        }
+    }
+}
--- app/src/main/java/artground/otterbear/com/artground/main/SplashActivity.kt
+++ app/src/main/java/artground/otterbear/com/artground/main/SplashActivity.kt
@@ -3,13 +3,31 @@ package artground.otterbear.com.artground.main
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import artground.otterbear.com.artground.R
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import android.widget.ImageView
+import java.util.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        //working1231233
-        //branch test
+
+        val imageView = findViewById<ImageView>(R.id.logo)
+        (imageView.getBackground() as AnimationDrawable).start()
+
+        val intent = Intent(this, PreperationActivity::class.java)
+        intent.putExtra("state", "launch")
+
+
+        // delay
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                startActivity(intent)
+                finish()
+            }
+        }, 1500)
+
     }
 }
