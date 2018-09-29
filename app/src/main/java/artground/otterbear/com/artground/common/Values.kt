dc9da752a008f16e471280252de63e6a495748f3
sumin jin <pistolcaffe@naver.com>
2018-09-29 19:08:59 +0900

    둘러보기 화면 작업

--- app/src/main/java/artground/otterbear/com/artground/common/Values.kt
+++ app/src/main/java/artground/otterbear/com/artground/common/Values.kt
@@ -7,5 +7,6 @@ class Values {
         const val API_ART_ITEM_IMAGE_URL_PREFIX = "HTTP://CULTURE.SEOUL.GO.KR"
         const val DUMMY_REVIEW_ITEM_ID: Long = -1
         const val EXTRA_ART_ITEM_LIST_OP_TYPE = "extra_art_item_list_op_type"
+        const val WALKTHROUGH_IDX = "walkthrough_idx"
     }
 }
--- app/src/main/java/artground/otterbear/com/artground/main/PreperationActivity.kt
+++ app/src/main/java/artground/otterbear/com/artground/main/PreperationActivity.kt
@@ -1,53 +1,91 @@
 package artground.otterbear.com.artground.main
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import artground.otterbear.com.artground.R
-import artground.otterbear.com.artground.preperationfragment.PreperationFragment1
-import artground.otterbear.com.artground.preperationfragment.PreperationFragment2
+import android.os.Handler
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
-import artground.otterbear.com.artground.preperationfragment.PreperationFragment3
-
-
-
-
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import artground.otterbear.com.artground.R
+import artground.otterbear.com.artground.common.Values
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import kotlinx.android.synthetic.main.activity_preperation.*
+import kotlinx.android.synthetic.main.fragment_walkthrough.*
 
 class PreperationActivity : AppCompatActivity() {
+
+    private val walkthroughBackgroundRes = arrayOf(R.drawable.category_bg_busking, R.drawable.category_bg_musical_opera, R.drawable.category_bg_festival)
+    private val autoPagingHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preperation)
+        loadWalkthroughBg(0)
 
-        val vp = findViewById(R.id.preperation_vp) as ViewPager
-        vp.setAdapter(pagerAdapter(supportFragmentManager))
-        vp.setCurrentItem(0)
+        viewPager.apply {
+            viewPager.adapter = WalkthroughPagerAdapter(supportFragmentManager)
+            addOnPageChangeListener(pageChangeListener)
+        }
 
+        startBtn.setOnClickListener {
+            startActivity(Intent(it.context, FavoriteCategory::class.java))
+            finish()
+        }
+        autoPagingHandler.postDelayed(autoPagingRunnable, 5000)
     }
 
+    override fun onDestroy() {
+        viewPager.removeOnPageChangeListener(pageChangeListener)
+        autoPagingHandler.removeCallbacks(autoPagingRunnable)
+        super.onDestroy()
+    }
 
-    private class pagerAdapter : FragmentStatePagerAdapter {
-        constructor(fm: android.support.v4.app.FragmentManager) : super(fm)
+    private val autoPagingRunnable = Runnable {
+        var nextPosition = viewPager.currentItem + 1
+        if (nextPosition >= 3) nextPosition = 0
+        viewPager.setCurrentItem(nextPosition, true)
+    }
 
-        override fun getItem(position: Int): android.support.v4.app.Fragment? {
-            when (position) {
-                0 -> {
-                    return PreperationFragment1()
-                }
-                1 -> {
-                    return PreperationFragment2()
-                }
-                2 -> {
-                    return PreperationFragment3()
-                }
-                else -> {
-                    return null
+    private fun loadWalkthroughBg(position: Int) {
+        GlideApp.with(this).load(walkthroughBackgroundRes[position]).transition(DrawableTransitionOptions.withCrossFade()).into(walkthroughBg)
+    }
+
+    private val pageChangeListener = object : ViewPager.SimpleOnPageChangeListener() {
+        override fun onPageSelected(position: Int) {
+            loadWalkthroughBg(position)
+            autoPagingHandler.removeCallbacks(autoPagingRunnable)
+            autoPagingHandler.postDelayed(autoPagingRunnable, 5000)
+        }
+    }
+
+    companion object {
+        class WalkthroughPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+            override fun getCount(): Int = 3
+            override fun getItem(position: Int): Fragment {
+                return WalkthroughFragment().apply {
+                    arguments = Bundle().apply { putInt(Values.WALKTHROUGH_IDX, position) }
                 }
             }
         }
 
-        override fun getCount(): Int {
-            return 3
+        class WalkthroughFragment : Fragment() {
+            override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+                return inflater.inflate(R.layout.fragment_walkthrough, container, false)
+            }
+
+            override fun onActivityCreated(savedInstanceState: Bundle?) {
+                super.onActivityCreated(savedInstanceState)
+                activity?.let { activity ->
+                    val idx = arguments!!.getInt(Values.WALKTHROUGH_IDX)
+                    title.text = activity.resources.getStringArray(R.array.walkthrough_titles)[idx]
+                    subTitle.text = activity.resources.getStringArray(R.array.walkthrough_descs)[idx]
+                }
+            }
         }
     }
 }
